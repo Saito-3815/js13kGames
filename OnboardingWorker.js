d01d5eba5124c8d1da44c7dcef27c0e5f69e6ce0
Saito-3815 <sai.engineer3815@gmail.com>
2024-10-19 13:21:23 +0900

    fix: manifestfail-name

--- OnboardingWorker.js
+++ OnboardingWorker.js
@@ -1,22 +1,22 @@
 // service-worker.js
-const CACHE_NAME = 'my-pwa-cache-v1';
+const CACHE_NAME = "my-pwa-cache-v3";
 const urlsToCache = [
-  '/',
-  '/onboarding.html',
-  '/onboarding.js',
-  'js13kpwa.webmanifest.onb',
+  // '/',
+  "/onboarding.html",
+  "/onboarding.js",
+  "js13kpwa.onbording.webmanifest",
 ];
 
-self.addEventListener('install', (event) => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request)
+    caches
+      .match(event.request)
       .then((response) => response || fetch(event.request))
   );
 });
